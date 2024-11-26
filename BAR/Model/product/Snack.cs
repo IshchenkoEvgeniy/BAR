cf0c63d96494735a5f657ece4b05b484154a8183
IshchenkoEvgeniy <ghjcnj11222gmail.com>
2024-11-26 10:32:32 +0200

    Разметка проекта

--- BAR/Model/product/Snack.cs
+++ BAR/Model/product/Snack.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BAR
+namespace BAR.Model
 {
-    internal class PROVERKA
+    internal class Snack
     {
-        //Проверка1 
     }
 }
--- BAR/Services/ValidationServic.cs
+++ BAR/Services/ValidationServic.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BAR
+namespace BAR.Services
 {
-    internal class PROVERKA
+    internal class ValidationServic
     {
-        //Проверка1 
     }
 }
